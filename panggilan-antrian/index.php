6d022d4dfd5d09278a63beedc406ccd1512564b6
SkyVam <yuskaputravito@gmail.com>
2024-01-15 09:36:00 +0700

    Hapus Data
    Menambahkan fitur hapus data

--- panggilan-antrian/index.php
+++ panggilan-antrian/index.php
@@ -144,12 +144,7 @@
       
       <div class="d-flex flex-column flex-md-row px-4 py-3 mb-4 bg-white rounded-2 shadow-sm justify-content-center">
         <!-- Hapus Data -->
-          <button type="button" class="btn btn-outline-danger">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
-            <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
-            </svg>
-            Hapus Data
-          </button>
+          <button id="delete" class="btn btn-danger btn-sm"> <i class="fa fa-trash"></i> Hapus Data</button>
       </div>
     </div>
 
@@ -269,6 +264,14 @@
           data: { id: id }            // tentukan data yang dikirim
         });
       });
+
+      // hapus semua data
+      $('#delete').on('click', function(){
+        $.ajax({
+            type: 'POST',
+            url: 'delete.php',
+          });
+        });
 
       // auto reload data antrian setiap 1 detik untuk menampilkan data secara realtime
       setInterval(function() {
