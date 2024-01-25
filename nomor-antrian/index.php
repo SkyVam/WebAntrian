d2806000ccb3379df57950dac4e5c4b01f764843
SkyVam <yuskaputravito@gmail.com>
2024-01-25 12:54:01 +0700

    update 4 loket
    4 loket tetapi masih 1 database

--- nomor-antrian/index.php
+++ nomor-antrian/index.php
@@ -50,12 +50,11 @@
 
           <div class="card border-0 shadow-sm">
             <div class="card-body text-center d-grid p-5">
-              <div class="border border-success rounded-2 py-2 mb-5">
-              <ul style="list-style-type:none;">
-              <li><button id="loket1" class="btn btn-success btn-block rounded-pill fs-5 px-5 py-4 mb-2">Loket 1</button></li>
-              <li><button id="loket2" class="btn btn-success btn-block rounded-pill fs-5 px-5 py-4 mb-2">Loket 2</button></li>
-              <li><button id="loket3" class="btn btn-success btn-block rounded-pill fs-5 px-5 py-4 mb-2">Loket 3</button></li>
-              <li><button id="loket4" class="btn btn-success btn-block rounded-pill fs-5 px-5 py-4 mb-2">Loket 4</button></li>
+              <div class="border border-success rounded-2 py-2">
+              <a href="loket1" id="loket1" class="btn btn-success btn-block rounded-pill fs-5 px-5 py-4 mb-2">Loket 1</a></br>
+              <a href="loket2" id="loket2" class="btn btn-success btn-block rounded-pill fs-5 px-5 py-4 mb-2">Loket 2</a></br>
+              <a href="loket3" id="loket3" class="btn btn-success btn-block rounded-pill fs-5 px-5 py-4 mb-2">Loket 3</a></br>
+              <a href="loket4" id="loket4" class="btn btn-success btn-block rounded-pill fs-5 px-5 py-4 mb-2">Loket 4</a></br>
               </ul>
               </div>
             </div>
@@ -80,28 +79,6 @@
   <!-- Popper and Bootstrap JS -->
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
-
-  <script type="text/javascript">
-    $(document).ready(function() {
-      // proses insert data
-      $("#loket1").click(function() {
-              $('#full').load('loket.php').fadeIn('slow');
-            });
-
-      $("#loket2").click(function() {
-                $('#full').load('loket.php').fadeIn('slow');
-              });
-
-      $("#loket3").click(function() {
-              $('#full').load('loket.php').fadeIn('slow');
-            });
-
-      $("#loket4").click(function() {
-              $('#full').load('loket.php').fadeIn('slow');
-            });
-        });
-      
-  </script>
 </body>
 
 </html>
